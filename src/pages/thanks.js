be52f8d9fe3ca16cbf82945b4c0f32db2322263e
Craig Dennis <craigmdennis@gmail.com>
2020-08-29 01:11:27 +0100

    Create thanks.js success page (#5)

---
+++ src/pages/thanks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const SubmissionSuccessPage = () => {
+  return (
+    <Layout>
+      <h1>Submitted</h1>
+    </Layout>
+  );
+};
+
+export default SubmissionSuccessPage;
