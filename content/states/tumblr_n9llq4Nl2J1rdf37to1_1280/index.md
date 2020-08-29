6f28c76fa3545d9bf91c0eacc75ae389e25a5ad3
Craig Dennis <craigmdennis@gmail.com>
2020-08-29 18:46:09 +0100

    Modified files with Netlify CMS

--- content/states/tumblr_n9llq4Nl2J1rdf37to1_1280/index.md
+++ content/states/tumblr_n9llq4Nl2J1rdf37to1_1280/index.md
@@ -1,4 +1,10 @@
 ---
-title: 
-image: ./tumblr_n9llq4Nl2J1rdf37to1_1280.png
+title: Nobody nearby in Airdrop for macOS
+date: 2020-08-29T17:43:09.467Z
+image: cleanshot-2020-08-29-at-18.29.19.png
+tags:
+  - macOS
+  - desktop
+  - app
+  - ""
 ---
--- src/pages/submit.js
+++ src/pages/submit.js
@@ -20,31 +20,28 @@ const UserSubmissionPage = () => {
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
               htmlFor="grid-first-name"
             >
-              First Name
+              Name
             </label>
             <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-first-name"
               type="text"
-              placeholder="Jane"
+              placeholder="Jane Doe"
             />
-            <p className="text-red-500 text-xs italic">
-              Please fill out this field.
-            </p>
           </div>
 
           <div className="w-full md:w-1/2 px-3">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-last-name"
+              htmlFor="grid-twitter"
             >
-              Last Name
+              Twitter Handle
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
+              id="grid-twitter"
               type="text"
-              placeholder="Doe"
+              placeholder="@emptystates"
             />
           </div>
         </div>
@@ -52,75 +49,48 @@ const UserSubmissionPage = () => {
           <div className="w-full px-3">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-password"
+              htmlFor="grid-file"
             >
-              Password
+              Screenshot
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-password"
-              type="password"
-              placeholder="******************"
+              id="grid-file"
+              type="file"
             />
             <p className="text-gray-600 text-xs italic">
-              Make it as long and as crazy as you&apos;d like
+              Please make sure it doesn&apos;t contain sensitive information.
             </p>
           </div>
         </div>
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-city"
+              htmlFor="grid-comment"
             >
-              City
+              Description
             </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-city"
-              type="text"
-              placeholder="Albuquerque"
+            <textarea
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              rows="3"
+              id="grid-comment"
             />
           </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3 mb-6 md:mb-0">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-state"
+              htmlFor="grid-twitter"
             >
-              State
-            </label>
-            <div className="relative">
-              <select
-                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-state"
-              >
-                <option>New Mexico</option>
-                <option>Missouri</option>
-                <option>Texas</option>
-              </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </div>
-            </div>
-          </div>
-          <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-zip"
-            >
-              Zip
+              Link to App / Website
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-zip"
+              id="grid-twitter"
               type="text"
-              placeholder="90210"
+              placeholder="https://emptystat.es"
             />
           </div>
         </div>
