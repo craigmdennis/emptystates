4cd4d37a5c17140fd998c1ae13e970d4a1c16240
Craig Dennis <craigmdennis@gmail.com>
2020-09-26 18:33:46 +0100

    Custom pagination controls

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,52 +1,85 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import styles from '../styles/pagination.module.css';
 import ChevrontLeft from '../images/icon-chevron-left.svg';
 import ChevrontRight from '../images/icon-chevron-right.svg';
 
-const Pagination = ({ currentPage, numPages, pathbase = '' }) => {
-  const Nav = ({ numPages, currentPage }) => {
-    let buttons = [];
+const PaginationButton = (props) => {
+  const { type, children } = props;
+  if (type === 'span') {
+    return (
+      <span className={styles.paginationButton} {...props}>
+        {children}
+      </span>
+    );
+  } else {
+    return (
+      <Link className={styles.paginationButtonEnabled} {...props}>
+        {children}
+      </Link>
+    );
+  }
+};
 
-    for (let i = 0; i < numPages; i++) {
-      const pageNumber = i + 1;
-      buttons.push(
-        <PaginationButton
-          key={pageNumber}
-          pageNumber={pageNumber}
-          isCurrent={pageNumber === currentPage ? true : false}
-        >
-          {pageNumber}
-        </PaginationButton>
-      );
-    }
+PaginationButton.propTypes = {
+  type: PropTypes.oneOf(['link', 'span']),
+  children: PropTypes.node.isRequired,
+};
 
-    return buttons;
-  };
+const Pagination = ({ currentPage, numPages, basePath = '' }) => {
+  let buttons = [];
 
-  const PaginationButton = ({ pageNumber, isCurrent, children }) => {
-    const pageLink = pageNumber === 1 ? '' : pageNumber;
+  const pages = Array.from({ length: numPages }, (_, i) => i + 1);
 
-    if (isCurrent) {
-      return (
-        <span className={styles.paginationButtonActive}>{pageNumber}</span>
-      );
-    } else {
-      return (
-        <Link
-          className={styles.paginationButtonEnabled}
-          to={`${pathbase}/${pageLink}`}
-        >
-          {children}
-        </Link>
-      );
-    }
-  };
+  const previousLink = `${basePath}/${
+    currentPage === 2 ? '' : currentPage - 1
+  }`;
+
+  const nextLink = `${basePath}/${
+    currentPage === numPages ? '' : currentPage + 1
+  }`;
 
   return (
     <nav className={styles.paginaton}>
-      <Nav numPages={numPages} currentPage={currentPage} />
+      <PaginationButton
+        type={currentPage === 1 ? 'span' : 'link'}
+        to={previousLink}
+        key="prev"
+      >
+        <ChevrontLeft className={styles.icon} />
+        <span className="visually-hidden">Previous Page</span>
+      </PaginationButton>
+
+      {/* Get the total number of pages
+      if more than 5
+      get the first first page,
+      the page before the current page,
+      the page after the current page,
+      and the last page */}
+      {pages.map((pageNumber, index) => {
+        const midPageLink =
+          pageNumber === 1 ? `${basePath}` : `${basePath}/${pageNumber}`;
+
+        return (
+          <PaginationButton
+            key={index}
+            to={midPageLink}
+            type={pageNumber === currentPage ? 'span' : 'link'}
+          >
+            {pageNumber}
+          </PaginationButton>
+        );
+      })}
+
+      <PaginationButton
+        type={currentPage === numPages ? 'span' : 'link'}
+        to={nextLink}
+        key="next"
+      >
+        <ChevrontRight className={styles.icon} />
+        <span className="visually-hidden">Next Page</span>
+      </PaginationButton>
     </nav>
   );
 };
@@ -56,5 +89,5 @@ export default Pagination;
 Pagination.propTypes = {
   currentPage: PropTypes.number.isRequired,
   numPages: PropTypes.number.isRequired,
-  pathbase: PropTypes.string,
+  basePath: PropTypes.string,
 };
